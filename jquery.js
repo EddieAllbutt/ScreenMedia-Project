a8a358057a6c40ffe5454696dbc9f4e7781dd7b8
EddieAllbutt <54983386+EddieAllbutt@users.noreply.github.com>
2019-09-12 16:38:01 +0100

    Retinkering with progress bar
    (progress bar still doesn't work)
    Unsure about tracking other significant edits

--- jquery.js
+++ jquery.js
@@ -1,4 +1,31 @@
-var winHeight = $(window).height(),
+let prog = document.getElementById("progress");
+
+let body = document.body,
+  html = document.documentElement;
+
+let height = Math.max(
+  body.scrollHeight,
+  body.offsetHeight,
+  html.clientHeight,
+  html.scrollHeight,
+  html.offsetHeight
+);
+
+const setProgress = () => {
+  let scrollFromTop =
+    (document.documentElement.scrollTop || body.scrollTop) + html.clientHeight;
+  let width = (scrollFromTop / height) * 100 + "%";
+
+  console.log("scroll", html.clientHeight, body.scrollTop);
+
+  prog.style.width = width;
+};
+
+window.addEventListener("scroll", setProgress);
+
+setProgress();
+
+/*var winHeight = $(window).height(),
   docheight = $(document).height();
 max = docheight - winHeight;
 
@@ -27,3 +54,4 @@ $(document).on("ready", function() {
     progressBar.attr("value", value);
   });
 });
+*/
